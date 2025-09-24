51d533cfeeb3293087d7a0c06619813f26c10045
KarlErichR <kermmlgs@gmail.com>
2025-09-24 20:42:48 +0300

    Done

--- Week2/A2_T5.py
+++ Week2/A2_T5.py
@@ -5,10 +5,10 @@ print(f"The word you inserted is '{Word}' and in reverse it is '{Word[::-1]}'.")
 print(f"The inserted word length is {len(Word)}")
 print(f"Last character is '{Word[-1]}'\n")
 
-print("Take substring from inserted word by inserting...")
+print("Take substring from the inserted word by inserting...")
 Start = int(input("1) Starting point: "))
 End = int(input("2) Ending point: "))
 Step = int(input("3) Step size: ")) 
 
-print(f"\nThe word '{Word}' sliced to defined substring is '{Word[Start:End:Step]}'.")
+print(f"\nThe word '{Word}' sliced to the defined substring is '{Word[Start:End:Step]}'.")
 print("Program ending.")
